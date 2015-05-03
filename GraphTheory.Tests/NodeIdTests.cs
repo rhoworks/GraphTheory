a4e7a772501b246f3c9013ed80bc122cde13ad51
Rodolfo Neuber <rneuber@bluebeam.com>
2015-05-02 22:17:45 -0700

    Added GraphTheory.Tests.csproj Updated the solution to restore nuget packages automatically.

---
+++ GraphTheory.Tests/NodeIdTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphTheory.Tests
+{
+    [TestFixture]
+    public class NodeIdTests
+    {
+        [Test]
+        public void New_Just_Works()
+        {
+        }
+    }
+}
