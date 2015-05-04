f016fdddf82f228baa8c58912da5fe77af9923c1
Rodolfo Neuber <rneuber@bluebeam.com>
2015-05-03 20:28:49 -0700

    Added new PCL test harness.

---
+++ GraphTheory.Tests/GraphIdTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GraphTheory.Tests
+{
+    [TestFixture]
+    public class GraphIdTests
+    {
+        [Test]
+        public void New_Just_Works()
+        {
+            dynamic g = new object(); // todo: use GraphId object
+            Assert.That(g, Is.Not.Null);
+        }
+    }
+}
